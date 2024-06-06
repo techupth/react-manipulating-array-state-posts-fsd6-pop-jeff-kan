b6771bcb3efb9daac9252f614bfb1a0e50e856d5
Jeffiros <krittiwat.c@gmail.com>
2024-06-06 16:10:39 +0700

    feat: fill up answer

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,31 +1,53 @@
+import { useState } from "react";
+import dataList from "./data"
+
 function Posts() {
+  
+  const [likes,setLikes] = useState(dataList.map((data) => data.likes));
+  
+  const handleLike = (index) => {
+    const newLikes = [...likes];
+    newLikes[index] += 1;
+    setLikes(newLikes);
+  };
+  const handleDislike = (index) => {
+    if(likes[index]>0){const newLikes = [...likes];
+    newLikes[index] -= 1;
+    setLikes(newLikes);}
+  };
+  
   return (
-    <div class="app-wrapper">
-      <h1 class="app-title">Posts</h1>
-      <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
+  <div class="app-wrapper">
+    <h1 class="app-title">Posts</h1>
+    {dataList.map((data,index) => { 
+      return (
+          <div class="post-list">
+            <div class="post-item">
+              <div class="post-header" key={index}>
+                <h2>Post Title #{data.id}</h2>
+                <div class="post-social-media-stats">
+                  <span class="stats-topic">Likes: </span>
+                  <span class="post-likes" key={index}>{likes[index]}</span>
+                </div>
+              </div>
+                <p class="post-content" key={index}>
+                  {data.content}
+                </p>
+              <div class="post-actions">
+                <button class="like-button " onClick={()=>{handleLike(index)}}>
+                  Like
+                </button>
+                <button class="dislike-button" onClick={()=>{handleDislike(index)}}>
+                  Dislike
+                </button>
+              </div>
             </div>
           </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+          );
+        })
+    }
+  </div>
+  )
 }
 
 export default Posts;
