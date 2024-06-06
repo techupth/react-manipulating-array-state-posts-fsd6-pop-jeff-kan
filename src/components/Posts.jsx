1632e9da0d7782fb5c5484f050b17e78dd0e171d
Kan <kanalovefootball@gmail.com>
2024-06-06 17:37:22 +0700

    feat: Add post with like and dislike button

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,27 +1,60 @@
+import postList from "../data/PostList";
+import { useState } from "react";
 function Posts() {
+  const [postData, setPostData] = useState(postList);
+  const handleLikePost = (postIndex) => {
+    const newPostData = [...postData];
+    newPostData[postIndex].likes = newPostData[postIndex].likes + 1;
+    setPostData(newPostData);
+  };
+
+  const handleDislikePost = (postIndex) => {
+    const newPostData = [...postData];
+    if (newPostData[postIndex].likes > 0) {
+      newPostData[postIndex].likes = newPostData[postIndex].likes - 1;
+      setPostData(newPostData);
+    }
+  };
+
   return (
     <div class="app-wrapper">
       <h1 class="app-title">Posts</h1>
       <div class="post-list">
-        <div class="post-item">
-          <div class="post-header">
-            <h2>Post Title #1</h2>
-            <div class="post-social-media-stats">
-              <span class="stats-topic">Likes: </span>
-              <span class="post-likes">10</span>
-            </div>
-          </div>
-          <p class="post-content">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
-            tincidunt, leo non molestie consectetur, elit libero faucibus
-            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
-            condimentum mauris euismod pellentesque eu eu justo...
-          </p>
-          <div class="post-actions">
-            <button class="like-button">Like</button>
-            <button class="dislike-button">Dislike</button>
-          </div>
+        <div class = "set-post-function">
+          {postList.map((post, index) => {
+            return (
+              <div class="post-item">
+                <div class="post-header" key={index}>
+                  <h2>{post.title}</h2>
+                  <div class="post-social-media-stats">
+                    <span class="stats-topic">Likes: </span>
+                    <span class="post-likes">{post.likes}</span>
+                  </div>
+                </div>
+                <p class="post-content" key={index}>
+                  {post.content}
+                </p>
+                <div class="post-actions">
+                  <button
+                    class="like-button"
+                    onClick={() => {
+                      handleLikePost(index);
+                    }}
+                  >
+                    Like
+                  </button>
+                  <button
+                    class="dislike-button"
+                    onClick={() => {
+                      handleDislikePost(index);
+                    }}
+                  >
+                    Dislike
+                  </button>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
